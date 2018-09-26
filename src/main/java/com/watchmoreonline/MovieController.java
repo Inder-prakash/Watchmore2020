01feea052b5e600d3f105d0c09ca55ff214213f4
inder prakash <inder.prakash@hotmail.com>
2018-09-26 14:32:26 +0530

    f

--- src/main/java/com/watchmoreonline/MovieController.java
+++ src/main/java/com/watchmoreonline/MovieController.java
@@ -1,5 +1,15 @@
 package com.watchmoreonline;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.sound.midi.Soundbank;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -22,6 +32,93 @@ public class MovieController {
 	
 	@Autowired
 	UserDao udao;
+	
+//	@GetMapping("/ml")
+//	public void ml() {
+//		try {
+//			
+//			File file = new File("C:\\Users\\Dovahkiin\\Desktop\\data\\embed.txt");
+//			FileReader fileReader = new FileReader(file);
+//			BufferedReader bufferedReader = new BufferedReader(fileReader);
+//			String line;
+//			List<String> embed = new  ArrayList<String>();
+//			while ((line = bufferedReader.readLine()) != null) {
+//				embed.add(line);
+//			}
+//			fileReader.close();			
+//			Collections.reverse(embed); 
+//
+//			
+//			
+//			file = new File("C:\\Users\\Dovahkiin\\Desktop\\data\\directlink.txt");
+//		    fileReader = new FileReader(file);
+//			bufferedReader = new BufferedReader(fileReader);
+//			List<String> directlink = new  ArrayList<String>();
+//			while ((line = bufferedReader.readLine()) != null) {
+//				directlink.add(line);
+//			}
+//			fileReader.close();		
+//			Collections.reverse(directlink); 
+//					
+//			file = new File("C:\\Users\\Dovahkiin\\Desktop\\data\\size.txt");
+//		    fileReader = new FileReader(file);
+//			bufferedReader = new BufferedReader(fileReader);
+//			List<String> size = new  ArrayList<String>();
+//			while ((line = bufferedReader.readLine()) != null) {
+//				size.add(line);
+//			}
+//			fileReader.close();	
+//			Collections.reverse(size); 
+//			
+//			
+//			
+//			file = new File("C:\\Users\\Dovahkiin\\Desktop\\data\\name.txt");
+//		    fileReader = new FileReader(file);
+//			bufferedReader = new BufferedReader(fileReader);
+//			List<String> name = new  ArrayList<String>();
+//			while ((line = bufferedReader.readLine()) != null) {
+//				name.add(line);
+//			}
+//			fileReader.close();	
+//			Collections.reverse(name); 
+//			
+//			
+//			
+//			file = new File("C:\\Users\\Dovahkiin\\Desktop\\data\\Image.txt");
+//		    fileReader = new FileReader(file);
+//			bufferedReader = new BufferedReader(fileReader);
+//			List<String> image = new  ArrayList<String>();
+//			while ((line = bufferedReader.readLine()) != null) {
+//				image.add(line);
+//			}
+//			fileReader.close();	
+//			Collections.reverse(image); 
+//			
+//			
+//			Movie m = new Movie();			
+//			for(int i=0; i<embed.size(); i++) {
+//
+//				m = new Movie();
+//				m.setDirectlink1080p("OpenLoad");
+//				m.setPlay1080p(embed.get(i));
+//				m.setDownload1080p(directlink.get(i));
+//				m.setSize1080p(size.get(i));
+//				m.setName(name.get(i));
+//				m.setImage(image.get(i));
+//				m.setLanguage("Hindi");
+//				m.setStatus("Public");
+//				mov.insert(m);
+//			}
+//			
+//
+//			
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+	
+	
 	
 	@PostMapping("/login")
 	public String login(@RequestBody String data) {
@@ -174,6 +271,75 @@ public class MovieController {
 		}	 	
 		return jarr;
 	}
+	
+	
+	@GetMapping("/hindiMovies")
+	public JSONArray hindiMovies() {	
+		JSONArray jarr = new JSONArray();
+		for(Movie m : mov.findAll()) {	
+			if(m.getStatus().equals("Public") && m.getLanguage().equals("Hindi")) {
+				JSONObject job = new JSONObject();	
+				job.put("Id",m.getId());		
+				job.put("Name",m.getName());
+				job.put("Image",m.getImage());
+				job.put("Directlink480p",m.getDirectlink480p());
+				job.put("Directlink720p",m.getDirectlink720p());
+				job.put("Directlink1080p",m.getDirectlink1080p());
+				job.put("Directlink4k",m.getDirectlink4k());
+				job.put("Play480p",m.getPlay480p());
+				job.put("Play720p",m.getPlay720p());
+				job.put("Play1080p",m.getPlay1080p());
+				job.put("Play4k",m.getPlay4k());
+				job.put("Download480p",m.getDownload480p());
+				job.put("Download720p",m.getDownload720p());
+				job.put("Download1080p",m.getDownload1080p());
+				job.put("Download4k",m.getDownload4k());
+				job.put("Size480p",m.getSize480p());
+				job.put("Size720p",m.getSize720p());
+				job.put("Size1080p",m.getSize1080p());
+				job.put("Size4k",m.getSize4k());
+				job.put("Language",m.getLanguage());
+				job.put("Status",m.getStatus());
+				jarr.add(job);
+			}
+		}	 	
+		return jarr;
+	}
+	
+	@GetMapping("/englishMovies")
+	public JSONArray englishMovies() {	
+		JSONArray jarr = new JSONArray();
+		for(Movie m : mov.findAll()) {	
+			if(m.getStatus().equals("Public") && m.getLanguage().equals("English") ) {
+				JSONObject job = new JSONObject();	
+				job.put("Id",m.getId());		
+				job.put("Name",m.getName());
+				job.put("Image",m.getImage());
+				job.put("Directlink480p",m.getDirectlink480p());
+				job.put("Directlink720p",m.getDirectlink720p());
+				job.put("Directlink1080p",m.getDirectlink1080p());
+				job.put("Directlink4k",m.getDirectlink4k());
+				job.put("Play480p",m.getPlay480p());
+				job.put("Play720p",m.getPlay720p());
+				job.put("Play1080p",m.getPlay1080p());
+				job.put("Play4k",m.getPlay4k());
+				job.put("Download480p",m.getDownload480p());
+				job.put("Download720p",m.getDownload720p());
+				job.put("Download1080p",m.getDownload1080p());
+				job.put("Download4k",m.getDownload4k());
+				job.put("Size480p",m.getSize480p());
+				job.put("Size720p",m.getSize720p());
+				job.put("Size1080p",m.getSize1080p());
+				job.put("Size4k",m.getSize4k());
+				job.put("Language",m.getLanguage());
+				job.put("Status",m.getStatus());
+				jarr.add(job);
+			}
+		}	 	
+		return jarr;
+	}
+	
+	
 	
 	@GetMapping("/PrivateMovies")
 	public JSONArray PrivateMovies() {	

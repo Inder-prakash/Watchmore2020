16bf99df7c858236eb331c80ccf6dd3d3b8a20f6
Inder <gamefort2017@gmail.com>
2019-10-23 20:27:46 +0530

    f

--- src/main/java/com/watchmoreonline/MovieController.java
+++ src/main/java/com/watchmoreonline/MovieController.java
@@ -2,13 +2,19 @@ package com.watchmoreonline;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sound.midi.Soundbank;
 
 import org.json.simple.JSONArray;
@@ -21,7 +27,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.watchmoreonline.moviebase.MovieBase;
 import com.watchmoreonline.moviebase.MovieBaseDao;
 import com.watchmoreonline.users.User;
@@ -37,14 +46,50 @@ public class MovieController {
 	@Autowired
 	UserDao udao;
 
+	
+	public String upload(HttpServletRequest req , String imageUrl) {
+		try {
+			String path2 = req.getRealPath("/");
+			String path = path2+"temp.jpg";
+			saveImage(imageUrl, path);
+			Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+                  	"cloud_name", "df3btb7v4",
+                 	"api_key", "144397625466591",
+                 	"api_secret", "_auyDn69x8adsIwxVwMRrljcx0U"));
+			File f = new File(path);
+			Map uploadResult = cloudinary.uploader().upload(f,ObjectUtils.emptyMap());
+			return uploadResult.get("secure_url").toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return "Failed";
+		}
+		
+	}
+	
+	
+	public static void saveImage(String imageUrl, String destinationFile) throws IOException {
+		URL url = new URL(imageUrl);
+		InputStream is = url.openStream();
+		OutputStream os = new FileOutputStream(destinationFile);
+		byte[] b = new byte[2048];
+		int length;
+		while ((length = is.read(b)) != -1) {
+			os.write(b, 0, length);
+		}
+		is.close();
+		os.close();
+	}
+	
+	
 	@PostMapping("/AddMovie")
-	public String addmovie(@RequestBody String data) throws ParseException {
+	public String addmovie(HttpServletRequest req ,@RequestBody String data) throws ParseException {
 		JSONParser jp = new JSONParser();		
 		JSONObject json = new JSONObject();	
 			JSONObject joObject = (JSONObject)jp.parse(data);	
 			MovieBase m = new MovieBase();
 			m.setName(joObject.get("Name").toString());
-			m.setImage(joObject.get("Image").toString());
+			m.setImage(upload(req, joObject.get("Image").toString()));
 			m.setLanguage(joObject.get("Language").toString());
 			m.setLink(joObject.get("Link").toString());
 			m.setSize(joObject.get("Size").toString());
